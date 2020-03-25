317bf944a909a50cd1b6f1ae2e5e55ba010eb647
mohsohail <sohail.momin1@gmail.com>
2020-03-25 13:57:56 +0530

    removing orders collection

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -3,10 +3,8 @@ const router = express.Router();
 
 const userRoutes = require('../routes/user.route');
 const productRoutes = require('./product.route');
-const orderRoutes = require('./order.route');
 
 router.use('/users/', userRoutes);
 router.use('/products/', productRoutes);
-router.use('/orders/', orderRoutes);
 
 module.exports = router;
