dd80ef2b03c8f906d297f85398cd631a07aad5a7
mohsohail <sohail.momin1@gmail.com>
2020-03-25 15:20:09 +0530

    adding skills route

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,7 @@
-import Home from './source/pages/Home/HomePage';
+import HomePage from './source/pages/Home/HomePage';
+import SkillPage from './source/pages/Skill/SkillPage';
 
-export default [{ path: '/', exact: true, component: Home }];
+export default [
+  { path: '/', exact: true, component: HomePage },
+  { path: '/skills', exact: true, component: SkillPage }
+];
